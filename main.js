8bec1fc22e304551f4a3634a51f9eed0cc3e03e3
ss100yan <32798618+ss100yan@users.noreply.github.com>
2018-12-10 01:06:59 -0500

    new

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
- var word = ["git","html","javascript","react","angular","css","sql"];
+ var word = ["git","html","javascript","react","angular","css","sql","code"];
  
+ var randomWord = word[Math.floor(word.length * Math.random())];
+
+console.log(randomWord);
+document.onkeyup = function (event) {
+    var keyPress = document.getElementById("keypress");
+
+    var letterPressed = event.key;
+};
+console.log(event);
