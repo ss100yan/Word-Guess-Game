0876f9136870dde358caa13d7805f4d735808683
ss100yan <32798618+ss100yan@users.noreply.github.com>
2018-12-11 15:20:27 -0500

    new

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,23 @@
- var word = ["git","html","javascript","react","angular","css","sql","code"];
+
+// Array containing game words
+
+var word = ["git","html","javascript","react","angular","css","sql","code"];
+
+//Function choosing the random word
  
  var randomWord = word[Math.floor(word.length * Math.random())];
 
 console.log(randomWord);
+
+//Array showing the guessed word
+
+var guessedWord = ["_","_","_","_"];
+var guessedWordInput = document.getElementById("guessWord");
+guessedWordInput.textContent = guessedWord;
+    
+
+//On key event function
+
 document.onkeyup = function (event) {
    
     var key = event.key;
