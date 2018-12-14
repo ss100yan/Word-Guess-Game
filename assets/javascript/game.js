210ddea12879aa1db40991f3c42caf2fbef319cf
ss100yan <32798618+ss100yan@users.noreply.github.com>
2018-12-14 14:16:09 -0500

    12/14/18

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,50 @@
 
-// Array containing game words
+// variables 
 
-var word = ["git","html","javascript","react","angular","css","sql","code"];
+var words = ["git","html","javascript","react","angular","css","sql","code","jquery","chrome"];
+var CurrentWord = words[Math.floor(words.length * Math.random())];
+var GuessRemainings = 10;
+var myLength=CurrentWord.length;
+var display=[myLength];
+var NordArray=CurrentWord.split("");
+var output=" ";
 
-//Function choosing the random word
- 
- var randomWord = word[Math.floor(word.length * Math.random())];
+console.log(CurrentWord);
 
-console.log(randomWord);
+//   NordArray.forEach({
+    
+var setup = function(){
 
-//Array showing the guessed word
+for ( var i=0; i < CurrentWord.length; i++ ){
+display[i] = "_ ";
+output = output + display[i];
+     }
+document.getElementById("game").textContent= output;
+console.log(output);
+output="";
 
-var guessedWord = ["_" , "_" , "_" , "_"];
 
+document.getElementById("GsRemaining").textContent = GuessRemainings;
+      };
 
-
+window.onload = function(){
+  setup();
+};
 
 //On key event function
 
-document.onkeyup = function (event) {
+document.onkeyup = function () {
    
     var key = event.key;
     
   var keyPressText = document.getElementById("keypress");
   keyPressText.textContent = key;
     
-  var n = randomWord.includes(key);
+  
   console.log(n);
-
-  document.getElementById("guessWord").innerHTML = guessedWord;
+}
+ 
   
     
 
-};
 
-//Wins
-
-
-//Number of Guesses Remaining
-
-
-
-
-//Letters Already Guessed
-              
